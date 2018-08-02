a5d0f8b7c59361ef6ac5d568324fb094351b1515
Anousone Kaseumsouk, Jean Jia and Lan Dam <anousone@>
2018-08-02 15:00:06 -0700

    Add helper functions and refacter codes.

--- spec/SudokuChecker-spec.js
+++ spec/SudokuChecker-spec.js
@@ -1,8 +1,10 @@
-import { sudokuChecker } from './../src/sudokuChecker.js';
+import { SudokuSolver } from './../src/sudokuChecker.js';
 
 describe('SudokuChecker', function() {
-  it('should check all row, column, and block to be 1-9', function(){
-    var board = [5,3,4,6,7,8,9,1,2,
+  let sudokuBoard;
+
+  beforeEach(function(){
+    let board = [5,3,4,6,7,8,9,1,2,
                  6,7,2,1,9,5,3,4,8,
                  1,9,8,3,4,2,5,6,7,
                  8,5,9,7,6,1,4,2,3,
@@ -11,47 +13,89 @@ describe('SudokuChecker', function() {
                  9,6,1,5,3,7,2,8,4,
                  2,8,7,4,1,9,6,3,5,
                  3,4,5,2,8,6,1,7,9];
-    expect(sudokuChecker(board)).toEqual(true);
+    sudokuBoard = new SudokuSolver(board);
+  });
+
+  it('should check if a value already exist in the row', function(){
+    let row = [0,3,4,6,0,8,0,1,0];
+    expect(sudokuBoard.checkValue(row, 7)).toEqual(true);
+  });
+  
+  it('should check all row, column, and block to be 1-9', function(){
+    expect(sudokuBoard.sudokuChecker()).toEqual(true);
+  });
+
+  it('should get the row given a row number', function(){
+    expect(sudokuBoard.getRow(5)).toEqual([4,2,6,8,5,3,7,9,1]);
+  });
+
+  it('should get the column given a column number', function(){
+    expect(sudokuBoard.getColumn(5)).toEqual([7,9,4,6,5,2,3,1,8]);
+  });
+
+  it('should get the block given a block number', function(){
+    expect(sudokuBoard.getBlock(5)).toEqual([7,6,1,8,5,3,9,2,4]);
   });
 
   it('should fail since block is not 1-9', function(){
-    var board = [1,2,3,4,5,6,7,8,9,
-                 2,3,4,5,6,7,8,9,1,
-                 3,4,5,6,7,8,9,1,2,
-                 4,5,6,7,8,9,1,2,3,
-                 5,6,7,8,9,1,2,3,4,
-                 6,7,8,9,1,2,3,4,5,
-                 7,8,9,1,2,3,4,5,6,
-                 8,9,1,2,3,4,5,6,7,
-                 9,1,2,3,4,5,6,7,8];
-    expect(sudokuChecker(board)).toEqual(false);
+    let testBoard = [1,2,3,4,5,6,7,8,9,
+                     2,3,4,5,6,7,8,9,1,
+                     3,4,5,6,7,8,9,1,2,
+                     4,5,6,7,8,9,1,2,3,
+                     5,6,7,8,9,1,2,3,4,
+                     6,7,8,9,1,2,3,4,5,
+                     7,8,9,1,2,3,4,5,6,
+                     8,9,1,2,3,4,5,6,7,
+                     9,1,2,3,4,5,6,7,8];
+    let sudokuTestBoard = new SudokuSolver(testBoard);
+    expect(sudokuTestBoard.sudokuChecker()).toEqual(false);
   });
 
   it('should fail since one row is not 1-9', function(){
-    var board = [1,2,3,4,5,6,7,8,9,
-                 2,3,4,5,6,7,8,9,1,
-                 3,4,5,6,7,8,9,1,2,
-                 4,5,6,7,8,9,1,2,3,
-                 5,6,7,8,9,1,2,3,4,
-                 6,7,8,9,1,2,3,4,5,
-                 7,8,9,1,2,3,4,5,6,
-                 8,9,1,2,3,4,5,6,7,
-                 9,9,9,9,9,9,9,9,8];
-    expect(sudokuChecker(board)).toEqual(false);
+    let testBoard = [1,2,3,4,5,6,7,8,9,
+                     2,3,4,5,6,7,8,9,1,
+                     3,4,5,6,7,8,9,1,2,
+                     4,5,6,7,8,9,1,2,3,
+                     5,6,7,8,9,1,2,3,4,
+                     6,7,8,9,1,2,3,4,5,
+                     7,8,9,1,2,3,4,5,6,
+                     8,9,1,2,3,4,5,6,7,
+                     9,9,9,9,9,9,9,9,8];
+    let sudokuTestBoard = new SudokuSolver(testBoard);
+    expect(sudokuTestBoard.sudokuChecker()).toEqual(false);
   });
 
   it('should fail since one column is not 1-9', function(){
-    var board = [1,2,3,4,5,6,7,8,9,
-                 2,3,4,5,6,7,8,1,9,
-                 3,4,5,6,7,8,2,1,9,
-                 4,5,6,7,8,3,1,2,9,
-                 5,6,7,8,4,1,2,3,9,
-                 6,7,8,5,1,2,3,4,9,
-                 7,8,6,1,2,3,4,5,9,
-                 8,7,1,2,3,4,5,6,9,
-                 8,1,2,3,4,5,6,7,9];
-    expect(sudokuChecker(board)).toEqual(false);
+    let testBoard = [1,2,3,4,5,6,7,8,9,
+                     2,3,4,5,6,7,8,1,9,
+                     3,4,5,6,7,8,2,1,9,
+                     4,5,6,7,8,3,1,2,9,
+                     5,6,7,8,4,1,2,3,9,
+                     6,7,8,5,1,2,3,4,9,
+                     7,8,6,1,2,3,4,5,9,
+                     8,7,1,2,3,4,5,6,9,
+                     8,1,2,3,4,5,6,7,9];
+    let sudokuTestBoard = new SudokuSolver(testBoard);
+    expect(sudokuTestBoard.sudokuChecker()).toEqual(false);
   });
+
+  it('should return array of savedLocations', function(){
+    let board = [0,3,4,6,7,8,9,1,2,
+                 6,7,2,1,9,5,3,4,8,
+                 0,9,8,3,4,2,5,6,7,
+                 8,5,9,7,6,1,4,2,3,
+                 0,2,6,8,5,3,7,9,1,
+                 7,1,3,9,2,4,8,5,6,
+                 0,6,1,5,3,7,2,8,4,
+                 2,8,7,4,1,9,6,3,5,
+                 0,4,5,2,8,6,1,7,9];
+    let sudokuBoard = new SudokuSolver(board);
+    let locations = [0, 18, 36, 54, 72];
+    expect(sudokuBoard.saveEmptyLocation()).toEqual(locations);
+  });
+});
+
+
 
   // it('should check all row, column, and block to be 1-9', function(){
   //   var board = [[[5,3,4], [6,7,2], [1,9,8]],
@@ -117,4 +161,3 @@ describe('SudokuChecker', function() {
   //                [8,1,2,3,4,5,6,7,9]];
   //   expect(sudokuChecker(board)).toEqual(true);
   // });
-});
--- src/SudokuChecker.js
+++ src/SudokuChecker.js
@@ -1,7 +1,85 @@
-export function sudokuChecker(sudokuArray){
-  const numbers = [1,2,3,4,5,6,7,8,9];
-  let row;
-  let n = 0;
+export class SudokuSolver {
+  constructor(sudokuBoard){
+    this.sudokuArray = sudokuBoard;
+  }
+
+  // check to see if there is a conflict, return false if value already exist.
+  checkValue(array, value) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === value) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  getRow(rowNumber) {
+    return this.sudokuArray.slice((rowNumber-1)*9, ((rowNumber-1)*9)+9);
+  }
+
+  getColumn(colNumber) {
+    let row = [];
+    for (let i = (colNumber - 1); i < this.sudokuArray.length; i+=9) {
+      row.push(this.sudokuArray[i]);
+    }
+    return row;
+  }
+
+  getBlock(jenny) {
+    let row = [];
+    let rowIndex = 0;
+    jenny--;
+    if (jenny < 6 && jenny > 2) {
+      rowIndex = 1;
+      jenny-=3;
+    } else if (jenny < 9 && jenny > 5) {
+      rowIndex = 2;
+      jenny-=6;
+    }
+    row = row.concat(this.sudokuArray.slice((jenny*3) + (rowIndex*27), (jenny*3)+ (rowIndex*27)+3)).concat(this.sudokuArray.slice((jenny*3)+ (rowIndex*27)+9, (jenny*3)+ (rowIndex*27)+12)).concat(this.sudokuArray.slice((jenny*3)+ (rowIndex*27)+18, (jenny*3)+ (rowIndex*27)+21));
+    return row;
+  }
+
+  sudokuChecker() {
+    const numbers = [1,2,3,4,5,6,7,8,9];
+    let row;
+
+    // test for row 1d array
+    for (let i = 1; i <= 9; i++) {
+      if(this.getRow(i).sort().join("") != numbers.join("")) {
+        return false;
+      }
+    }
+
+    //test for column 1d array
+    for (let i = 1; i < 9; i++) {
+      if(this.getColumn(i).sort().join("") != numbers.join("")) {
+        return false;
+      }
+    }
+
+    //test for block
+    for (let i = 1; i < 9; i++) {
+      if(this.getBlock(i).sort().join("") != numbers.join("")) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  saveEmptyLocation() {
+    let storedLocations = [];
+    for (let i =0; i < this.sudokuArray.length; i++) {
+      if(this.sudokuArray[i] === 0){
+        storedLocations.push(i);
+      }
+    }
+    return storedLocations;
+  }
+}
+
+// Other ways to check
 
   // // test for row 2d array
   // for(let i = 0; i < sudokuArray.length; i++) {
@@ -21,35 +99,8 @@ export function sudokuChecker(sudokuArray){
   //   }
   // }
 
-  // test for row 1d array
-  for (let i = 0; i < sudokuArray.length; i+= 9) {
-    row = sudokuArray.slice(i,i+9);
-    if(row.sort().join("") != numbers.join("")){
-      return false;
-    }
-  }
-
-  //test for column 1d array
-  for (let i = 0; i < 9; i++) {
-    row = [];
-    for (let j = i; j < sudokuArray.length; j+= 9)
-    row.push(sudokuArray[j]);
-    if(row.sort().join("") != numbers.join("")){
-      return false;
-    }
-  }
-
-  //test for block
-  for (let i = 0; i < 9; i += 3) {
-    for (let j = i; j < sudokuArray.length; j += 27) {
-      row = [];
-      row = row.concat(sudokuArray.slice(j, j+3)).concat(sudokuArray.slice(j+9, j+12)).concat(sudokuArray.slice(j+18, j+21));
-      if(row.sort().join("") != numbers.join("")){
-        return false;
-      }
-    }
-  }
   // // test for block, 3d array
+  // let n = 0;
   // for (let i = 0; i < sudokuArray.length; i++) {
   //   n = 0
   //   for (let j = 0; j < 3; j++) {
@@ -62,6 +113,3 @@ export function sudokuChecker(sudokuArray){
   //     return false;
   //   }
   // }
-
-  return true;
-}
