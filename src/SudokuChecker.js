e2a0dccbce111ab762487112c44682ac38b2137e
Anousone Kaseumsouk, Jean Jia and Lan Dam <lan@>
2018-08-02 09:29:32 -0700

    Modify from 2D to 1D sudokuArray. Keeping 2D and 3D in comments.

--- src/SudokuChecker.js
+++ src/SudokuChecker.js
@@ -1,9 +1,9 @@
 export function sudokuChecker(sudokuArray){
   const numbers = [1,2,3,4,5,6,7,8,9];
-  let row = new Array(9);
+  let row;
   let n = 0;
 
-  // // test for row
+  // // test for row 2d array
   // for(let i = 0; i < sudokuArray.length; i++) {
   //   row = sudokuArray[i].slice();
   //   if(row.sort().join("") != numbers.join("")) {
@@ -11,7 +11,7 @@ export function sudokuChecker(sudokuArray){
   //   }
   // }
 
-  // // test for column
+  // // test for column 2d array
   // for (let i = 0; i < sudokuArray.length; i++) {
   //   for (let j = 0; j < sudokuArray.length; j++) {
   //     row[j] = sudokuArray[j][i];
@@ -21,33 +21,48 @@ export function sudokuChecker(sudokuArray){
   //   }
   // }
 
-  // test for column
-  for (let i = 0; i < 9; i++) {
-    n = 0
-    for (let j = 0; j < 3; j++) {
-      for (let k = 0; k < 3; k++) {
-        row[n] = sudokuArray[i][j][k];
-        n++;
-      }
-    }
+  // test for row 1d array
+  for (let i = 0; i < sudokuArray.length; i+= 9) {
+    row = sudokuArray.slice(i,i+9);
     if(row.sort().join("") != numbers.join("")){
       return false;
     }
   }
 
-  // test for block
-  for (let i = 0; i < sudokuArray.length; i++) {
-    n = 0
-    for (let j = 0; j < 3; j++) {
-      for (let k = 0; k < 3; k++) {
-        row[n] = sudokuArray[i][j][k];
-        n++;
-      }
-    }
+  //test for column 1d array
+  for (let i = 0; i < 9; i++) {
+    row = [];
+    for (let j = i; j < sudokuArray.length; j+= 9)
+    row.push(sudokuArray[j]);
     if(row.sort().join("") != numbers.join("")){
       return false;
     }
   }
+
+  //test for block
+  for (let i = 0; i < 9; i += 3) {
+    for (let j = i; j < sudokuArray.length; j += 27) {
+      row = [];
+      row = row.concat(sudokuArray.slice(j, j+3)).concat(sudokuArray.slice(j+9, j+12)).concat(sudokuArray.slice(j+18, j+21));
+      console.log(row);
+      if(row.sort().join("") != numbers.join("")){
+        return false;
+      }
+    }
+  }
+  // // test for block, 3d array
+  // for (let i = 0; i < sudokuArray.length; i++) {
+  //   n = 0
+  //   for (let j = 0; j < 3; j++) {
+  //     for (let k = 0; k < 3; k++) {
+  //       row[n] = sudokuArray[i][j][k];
+  //       n++;
+  //     }
+  //   }
+  //   if(row.sort().join("") != numbers.join("")){
+  //     return false;
+  //   }
+  // }
 
   return true;
 }
